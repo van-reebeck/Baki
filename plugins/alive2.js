f5da9efe8fe053ae0fc07a3309e20087f899f910
TSHEPHANG-V <100064063+TSHEPHANG-V@users.noreply.github.com>
2022-03-01 16:22:08 +0200

    Update alive2.js

--- plugins/alive2.js
+++ plugins/alive2.js
@@ -20,7 +20,7 @@ var split = Config.KAZTRO_SER.split('/');
 
 let whb = Config.WORKTYPE == 'public' ? false : true
 
-Kaztroser.addCommand({pattern: 'alive', fromMe: whb, dontAddCommandList: true}, (async (message, match) => {
+Kaztroser.addCommand({pattern: 'awake', fromMe: whb, dontAddCommandList: true}, (async (message, match) => {
 // send a buttons message!credit Ajayan
     const buttons = [
         {buttonId: 'id1', buttonText: {displayText: sk1 }, type: 1},
