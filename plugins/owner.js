8daa65f3e8aab5c289b9bd70bfee65b739bc5114
TSHEPHANG-V <100064063+TSHEPHANG-V@users.noreply.github.com>
2022-03-02 21:54:02 +0200

    Update owner.js

--- plugins/owner.js
+++ plugins/owner.js
@@ -12,7 +12,7 @@ if (Config.WORKTYPE == 'private') {
         }
 
         if (Config.OWNER == 'default') {
-            await message.client.sendMessage(message.jid,'*Kaztroser by Ajayan*' , MessageType.text);
+            await message.client.sendMessage(message.jid,'*Baki by Tshephang*' , MessageType.text);
         }
         else {
             await message.client.sendMessage(message.jid,Config.OWNER + '\n\n➠', MessageType.text);
